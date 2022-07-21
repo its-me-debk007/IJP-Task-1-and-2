098680f77dfacd7568d727eb0ea4450d9643d2d1
Debashish Kundu <debashish.joy@gmail.com>
2022-07-21 20:38:04 +0530

    duplicating slider feature successfully implemented

--- app/src/main/java/in/project/task1/adapter/SliderRecyclerAdapter.kt
+++ app/src/main/java/in/project/task1/adapter/SliderRecyclerAdapter.kt
@@ -1,41 +1,72 @@
 package `in`.project.task1.adapter
 
-import `in`.project.task1.databinding.ActivityMainBinding
 import `in`.project.task1.databinding.ItemSliderBinding
+import `in`.project.task1.model.SliderValues
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.slider.Slider
 import com.google.android.material.textview.MaterialTextView
 
-class SliderRecyclerAdapter(private val listener: SliderInterface, private val noOfSliders: Int) :
-	RecyclerView.Adapter<SliderViewHolder>() {
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder =
-		SliderViewHolder(ItemSliderBinding.inflate(LayoutInflater.from(parent.context),
-													 parent, false))
+class SliderRecyclerAdapter(private val listener: SliderInterface,
+							private var items: MutableList<SliderValues>) :
+	RecyclerView.Adapter<SliderRecyclerAdapter.SliderViewHolder>() {
 
-	override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
-			holder.binding.slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
-				override fun onStartTrackingTouch(slider: Slider) {
-					listener.onSliderStart(slider, holder.binding.minValue, holder.binding.maxValue)
-				}
+	inner class SliderViewHolder(val binding: ItemSliderBinding) :
+		RecyclerView.ViewHolder(binding.root) {
+		init {
+			binding.apply {
+				slider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+					override fun onStartTrackingTouch(slider: Slider) {
+						listener.onSliderStart(minValue, adapterPosition)
+					}
 
-				override fun onStopTrackingTouch(slider: Slider) {
-					listener.onSliderStop(slider, holder.binding.minValue, holder.binding.maxValue)
+					override fun onStopTrackingTouch(slider: Slider) {
+						listener.onSliderStop(slider, minValue, maxValue, adapterPosition)
+					}
+				})
+
+				slider.addOnChangeListener { it, _, _ ->
+					listener.onSliderChange(it, maxValue, adapterPosition)
 				}
-			})
+			}
+		}
 	}
 
-	override fun getItemCount(): Int = noOfSliders
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder =
+		SliderViewHolder(ItemSliderBinding.inflate(LayoutInflater.from(parent.context),
+												   parent, false))
 
-}
+	override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
+		holder.binding.apply {
+			minValue.text = items[position].startValue.toString()
+			maxValue.text = items[position].endValue.toString()
+			slider.valueFrom = items[position].startValue.toFloat()
+			slider.valueTo = items[position].endValue.toFloat()
+			slider.value = items[position].endValue.toFloat()
+			maxValue.setBackgroundColor(items[position].color)
+			minValue.setBackgroundColor(items[position].color)
+			slider.thumbTintList = ColorStateList.valueOf(items[position].color)
+			slider.trackActiveTintList = ColorStateList.valueOf(items[position].color)
+		}
+	}
 
-class SliderViewHolder(val binding: ItemSliderBinding) : RecyclerView.ViewHolder(binding.root) {
+	override fun getItemCount(): Int = items.size
 
+	@SuppressLint("NotifyDataSetChanged")
+	fun setSliders(newItems: List<SliderValues>) {
+		items = newItems.toMutableList()
+		notifyDataSetChanged()
+	}
 }
 
 interface SliderInterface {
-	fun onSliderStart(slider: Slider, minValue: MaterialButton, maxValue: MaterialButton)
-	fun onSliderStop(slider: Slider, minValue: MaterialButton, maxValue: MaterialButton)
+	fun onSliderStart(minValue: MaterialTextView, position: Int)
+
+	fun onSliderStop(slider: Slider, minValue: MaterialTextView,
+					 maxValue: MaterialTextView, position: Int)
+
+	fun onSliderChange(slider: Slider, maxValue: MaterialTextView, position: Int)
 }
--- app/src/main/java/in/project/task1/ui/activity/MainActivity.kt
+++ app/src/main/java/in/project/task1/ui/activity/MainActivity.kt
@@ -4,38 +4,85 @@ import `in`.project.task1.R
 import `in`.project.task1.adapter.SliderInterface
 import `in`.project.task1.adapter.SliderRecyclerAdapter
 import `in`.project.task1.databinding.ActivityMainBinding
+import `in`.project.task1.model.SliderValues
 import `in`.project.task1.viewModel.SliderViewModel
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.slider.Slider
 import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity(), SliderInterface {
 	private val viewModel by lazy { ViewModelProvider(this)[SliderViewModel::class.java] }
-
-	//	private val viewModel : SliderViewModel by viewModels()
-	private val adapter by lazy { SliderRecyclerAdapter(this, viewModel.sliderCount) }
+	private lateinit var toast: Toast
+	private val adapter by lazy { SliderRecyclerAdapter(this, viewModel.sliderValuesList) }
 	private lateinit var binding: ActivityMainBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+		toast = Toast.makeText(this, null, Toast.LENGTH_SHORT)
 		binding.sliderRecyclerView.adapter = adapter
 	}
 
-	override fun onSliderStart(slider: Slider,
-							   minValue: MaterialButton,
-							   maxValue: MaterialButton) {
-		minValue.setIconResource(R.drawable.ic_delete_sweep)
-		minValue.text = ""
+	override fun onSliderStart(minValue: MaterialTextView, position: Int) {
+		val icon = if (position == 0) R.drawable.ic_delete_sweep else R.drawable.ic_delete
+		minValue.setCompoundDrawablesWithIntrinsicBounds(icon, 0, 0, 0)
+		minValue.text = null
 	}
 
-	override fun onSliderStop(slider: Slider,
-							  minValue: MaterialButton,
-							  maxValue: MaterialButton) {
-		TODO("Not yet implemented")
+	override fun onSliderStop(slider: Slider, minValue: MaterialTextView,
+							  maxValue: MaterialTextView, position: Int) {
+
+		minValue.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+		minValue.text = slider.valueFrom.toInt().toString()
+
+		if (slider.value == slider.valueFrom) {
+			if (position == 0) {
+				viewModel.sliderValuesList.clear()
+				viewModel.sliderValuesList.add(SliderValues(1, 100, Color.parseColor("#DC143C")))
+			} else {
+				viewModel.sliderValuesList[position - 1].endValue =
+					viewModel.sliderValuesList[position].endValue
+				viewModel.sliderValuesList.removeAt(position)
+			}
+			adapter.setSliders(viewModel.sliderValuesList)
+			return
+		}
+
+		if (slider.valueTo - slider.value <= 2) {
+			toast.cancel()
+			toast = Toast.makeText(this, "Minimum segment length is 2!", Toast.LENGTH_SHORT)
+			toast.show()
+			slider.value = slider.valueTo
+			return
+		}
+
+		viewModel.sliderValuesList[position].endValue = slider.value.toInt()
+		val color = viewModel.sliderValuesList[position].color
+		viewModel.sliderValuesList.add(position + 1, SliderValues(slider.value.toInt() + 1,
+																  slider.valueTo.toInt(), color))
+		adapter.setSliders(viewModel.sliderValuesList)
+		slider.valueTo = slider.value
+	}
+
+	override fun onSliderChange(slider: Slider, maxValue: MaterialTextView, position: Int) {
+
+		if (slider.value == slider.valueFrom) {
+			val icon = if (position == 0) R.drawable.ic_delete_sweep else R.drawable.ic_delete
+			maxValue.setCompoundDrawablesWithIntrinsicBounds(icon, 0, 0, 0)
+			maxValue.text = null
+		} else {
+			maxValue.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+			maxValue.text = slider.value.toInt().toString()
+		}
+	}
+
+	override fun onPause() {
+		super.onPause()
+		toast.cancel()
 	}
 }
